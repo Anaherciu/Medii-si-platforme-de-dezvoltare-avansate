eb87b426e95d184d16bcc993fbf598e4eea38a9f
Anaherciu <ana.herciu@ulbsibiu.ro>
2024-03-29 21:59:34 +0200

    Initial project

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnimalSounds
+{
+    class Animal
+    {
+        public string Type { get; set; }
+        public string Sound { get; set; }
+        public Animal(string type, string sound)
+        {
+            Type = type;
+            Sound = sound;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine($"{Type} makes sound: {Sound}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>
+            {
+                new Animal("Cat", "Meow"),
+                new Animal("Dog", "Woof"),
+                new Animal("Cow", "Moo")
+            };
+
+            foreach (var animal in animals)
+            {
+                animal.MakeSound();
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
